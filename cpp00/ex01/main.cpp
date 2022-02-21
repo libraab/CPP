3bb75e460dfdf67a76fae8910cb32136790bc33b
libraab <asmabouhlelboughdira@hotmail.com>
2022-02-21 18:48:31 +0100

    still on ex01 -cpp00

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -7,59 +7,80 @@ void	ft_enter_data(contact *contact)
 	int		x;
 	static int	contact_nbr;
 
-	std::cout << "Please enter a command (ADD, SEARCH, EXIT) --> ";
-	std::getline(std::cin, cmd);
-
-	if (cmd == "SEARCH")
+	contact_nbr = 0;
+	while (1)
 	{
-		std::cout << "Who are you looking for ?" << std::endl;
-		std::cout << "Please enter a number between 1 & 8 --> ";
-		std::getline(std::cin, tmp);
-		std::cout << "_________________________________________________" << std::endl;
-		std::cout << "| FIRST NAME |" << contact[tmp - 1].ft_get_first_name() << std::endl;
-		std::cout << "_________________________________________________" << std::endl;
-	}
+		std::cout << std::endl << "Please enter a command (ADD, SEARCH, EXIT) --> ";
+		std::getline(std::cin, cmd);
 
-	else if (cmd == "ADD")
-	{
-		if (contact_nbr == 8)
-			std::cout << "🚫 NO MORE SPACE 🚫" << std::endl;
-		else
+		if (cmd == "SEARCH")
 		{
-			std::cout << "Please enter first name --> ";
-			std::getline(std::cin, tmp);
-			contact[contact_nbr].ft_stock_first_name(tmp);
-
-			std::cout << "Please enter last name --> ";
-			std::getline(std::cin, tmp);
-			contact[contact_nbr].ft_stock_last_name(tmp);
-
-			std::cout << "Please enter nickname --> ";
-			std::getline(std::cin, tmp);
-			contact[contact_nbr].ft_stock_nickname(tmp);
-
-			std::cout << "Please enter phone number --> ";
-			std::getline(std::cin, tmp);
-			contact[contact_nbr].ft_stock_phone_nbr(tmp);
-
-			std::cout << "Please enter darkest secret --> ";
-			std::getline(std::cin, tmp);
-			contact[contact_nbr].ft_stock_secret(tmp);
-
-			contact_nbr++;
+			std::cout << std::endl << "Who are you looking for ?" << std::endl << std::endl;
+			std::cout << "Please enter a number between 1 & 8 --> ";
+			std::cin >> x;
+			std::cout << "________________________________________" << std::endl;
+			std::cout << "|INDEX|FIRST NAME| LAST NAME|  NICKNAME|" << std::endl;
+			std::cout << "________________________________________" << std::endl;
+			std::cout << "|  " << x << "  |";
+			if (contact[x - 1].ft_get_first_name().length() < 10)
+				std::cout << std::setw(10) << contact[x - 1].ft_get_first_name() << "|";
+			else
+			{
+				for (int j = 0 ; j < 9 ; j++)
+					std::cout << contact[x - 1].ft_get_first_name()[j];
+				std::cout << ".|";
+			}
+			if (contact[x - 1].ft_get_last_name().length() < 10)
+				std::cout << std::setw(10) << contact[x - 1].ft_get_last_name() << "|";
+			if (contact[x - 1].ft_get_nickname().length() < 10)
+				std::cout << std::setw(10) << contact[x - 1].ft_get_nickname() << "|" << std::endl;
+			std::cout << "________________________________________" << std::endl;
+			
+			continue;
 		}
+	
+		else if (cmd == "ADD")
+		{
+			if (contact_nbr == 8)
+			{
+				std::cout << "🚫 NO MORE SPACE 🚫" << std::endl;
+				std::cout << "Need to let go of one" << std::endl;
+			}
+			else
+			{
+				std::cout << std::endl << "Enter first name --> ";
+				std::getline(std::cin, tmp);
+				contact[contact_nbr].ft_stock_first_name(tmp);
+
+				std::cout << "Enter last name --> ";
+				std::getline(std::cin, tmp);
+				contact[contact_nbr].ft_stock_last_name(tmp);
+	
+				std::cout << "Enter nickname --> ";
+				std::getline(std::cin, tmp);
+				contact[contact_nbr].ft_stock_nickname(tmp);
+
+				std::cout << "Enter phone number --> ";
+				std::getline(std::cin, tmp);
+				contact[contact_nbr].ft_stock_phone_nbr(tmp);
+
+				std::cout << "Enter darkest secret --> ";
+				std::getline(std::cin, tmp);
+				contact[contact_nbr].ft_stock_secret(tmp);
+				std::cout << std::endl << "CONTACT ADDED ✅" << std::endl;
+		
+				contact_nbr++;
+			}
+			continue;
+		}
+
+		else if (cmd == "EXIT")
+		{
+			std::cout << "Bye hun 😘" << std::endl;
+			exit(0);
+		}
+		continue;
 	}
-
-	else if (cmd == "EXIT")
-	{
-		std::cout << "Bye hun 😘" << std::endl;
-		exit(0);
-	}
-
-	else
-		std::cout << "⛔️ WRONG COMMAND ⛔️" << std::endl;
-
-	return;
 }
 
 int main(int ac, char **av)
@@ -67,7 +88,6 @@ int main(int ac, char **av)
 	(void) av;
 	contact contact[8];
 	if (ac == 1)
-		while(1)
-			ft_enter_data(contact);
+		ft_enter_data(contact);
 	return (0);
 }
