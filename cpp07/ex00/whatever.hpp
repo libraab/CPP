1976f8e67a4d67dbfb2497238d724db88c62ecf7
Asma Bouhlel boughdira <abouhlel@c1r4p2.42nice.fr>
2022-04-13 11:35:46 +0200

    going home cause im sick of it and im litteraly sick

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,9 +6,20 @@
 /*   By: abouhlel <abouhlel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 09:59:49 by abouhlel          #+#    #+#             */
-/*   Updated: 2022/04/13 09:59:55 by abouhlel         ###   ########.fr       */
+/*   Updated: 2022/04/13 11:34:45 by abouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
+#define swap
+#define max
+#define min
+
+class whatever
+{
+    public:
+        whatever(void);
+        ~whatever(void);
+};
